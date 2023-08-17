636a60c44f5eee0eb8d3f64c623699ee8155b6b9
Aulas Ingeniería <aulasingenieria@12C805E06203024.JAVERIANA.LOC>
2023-08-17 11:55:17 -0500

    Fin de clase Spinner

---
+++ app/src/main/java/com/example/app_2330/PantallaConstraint.kt
@@ -0,0 +1,11 @@
+package com.example.app_2330
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaConstraint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_constraint)
+    }
+}
